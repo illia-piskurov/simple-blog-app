faac2c8dd1dd7ed54cc6969e0148cbe706ce1aa1
Illia Piskurov <piskryov@gmail.com>
2025-04-29 12:13:12 +0300

    cors settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,15 +3,21 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { setupSwagger } from './utils/swagger.util';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
+
   app.use(cookieParser());
+
   app.useGlobalPipes(new ValidationPipe());
+
   app.enableCors(
     {
-      origin: 'http://localhost:3001',
-      methods: 'GET,POST,UPDATE,DELETE,PATCH',
+      origin: config.getOrThrow<string>('ALLOWRD_ORIGINS'),
+      methods: '*',
       credentials: true,
     }
   );
